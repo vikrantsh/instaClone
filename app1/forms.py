a84f02729aade688b0f4ff4aef4f00ca0726a2b6
vikrant <vik.rao17@gmail.com>
2017-07-17 15:02:52 +0530

    signup

---
+++ app1/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from models import UserModel
+
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields=['email','username','name','password']
---
+++ app1/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from datetime import datetime
+
+from django.shortcuts import render, redirect
+from forms import SignUpForm
+from models import UserModel
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+
+# Create your views here.
+
+def signup_view(request):
+    if request.method == "POST":
+        print 'Sign up form submitted'
+    elif request.method == 'GET':
+        form = SignUpForm()
+
+    return render(request, 'index.html', {'form' : form})
